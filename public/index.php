ee88e1001a4c633366d080f8983cebcc0619973d
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-11-05 18:08:03 +0400

    Router refractor to class

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 require_once '../helpers.php';
+require_once basePath('Router.php');
 
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-require_once basePath('router.php');
+$router = new Router();
+$routes = require_once basePath('routes.php');
+
+$router->route($uri, $method);
