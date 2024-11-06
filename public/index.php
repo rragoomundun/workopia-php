9394e7000be2cd2b1eeca70bfaba3907b87b99f8
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-11-06 17:50:06 +0400

    Single listing and named params

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ $router = new Router();
 $routes = require_once basePath('routes.php');
 
 // Get current uri and http method
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Route the request
