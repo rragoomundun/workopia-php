c3b34ee5a826ad4f388d852fe6d91f239193aea2
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-11-06 10:00:01 +0400

    Query method and fetch listings

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 require_once '../helpers.php';
-
 require_once basePath('Router.php');
 require basePath('Database.php');
 
@@ -8,10 +7,15 @@ $config = require basePath('config/db.php');
 
 $db = new Database($config);
 
+// Instantiating the router
+$router = new Router();
+
+// Get routes
+$routes = require_once basePath('routes.php');
+
+// Get current uri and http method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
-$router = new Router();
-$routes = require_once basePath('routes.php');
-
+// Route the request
 $router->route($uri, $method);
