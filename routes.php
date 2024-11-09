d0d1d4d710738ae8a34813b4563a48985506ebc6
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-11-09 15:43:36 +0400

    Controller Classes - Home & Listings

--- routes.php
+++ routes.php
@@ -1,5 +1,9 @@
 <?php
 $router->get('/', 'HomeController@index');
+$router->get('/listings', 'ListingController@index');
+$router->get('/listings/create', 'ListingController@create');
+$router->get('/listing', 'ListingController@show');
+
 
 // $router->get('/', 'controllers/home.php');
 // $router->get('/listings', 'controllers/listings/index.php');
