6d4ba3ae99581c99e3f9760e068bc489b41afd14
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-11-12 17:44:07 +0400

    Edit form

--- App/Controllers/ListingController.php
+++ App/Controllers/ListingController.php
@@ -167,4 +167,31 @@ class ListingController
 
     redirect('/listings');
   }
+
+  /**
+   * Show the listing edit form
+   *
+   * @param array $params
+   * @return void
+   */
+  public function edit($params)
+  {
+    $id = $params['id'] ?? '';
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    // Check if listing exists
+    if (!$listing) {
+      ErrorController::notFound('Listing not found');
+      return;
+    }
+
+    loadView('listings/edit', [
+      'listing' => $listing
+    ]);
+  }
 }
--- App/views/listings/edit.view.php
+++ App/views/listings/edit.view.php
@@ -5,7 +5,7 @@
 <!-- Post a Job Form Box -->
 <section class="flex justify-center items-center mt-20">
   <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
-    <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
+    <h2 class="text-4xl text-center font-bold mb-4">Edit Job Listing</h2>
     <form method="POST" action="/listings">
       <h2 class=" text-2xl font-bold mb-6 text-center text-gray-500">
         Job Info
@@ -25,14 +25,14 @@
           name="title"
           placeholder="Job Title"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['title'] ?? '' ?>" />
+          value="<?= $listing->title ?? '' ?>" />
       </div>
       <div class="mb-4">
         <textarea
           name="description"
           placeholder="Job Description"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['description'] ?? '' ?>"></textarea>
+          value="<?= $listing->description ?? '' ?>"></textarea>
       </div>
       <div class="mb-4">
         <input
@@ -40,7 +40,7 @@
           name="salary"
           placeholder="Annual Salary"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['salary'] ?? '' ?>" />
+          value="<?= $listing->salary ?? '' ?>" />
       </div>
       <div class="mb-4">
         <input
@@ -48,7 +48,7 @@
           name="requirements"
           placeholder="Requirements"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['requirements'] ?? '' ?>" />
+          value="<?= $listing->requirements ?? '' ?>" />
       </div>
       <div class="mb-4">
         <input
@@ -56,7 +56,7 @@
           name="benefits"
           placeholder="Benefits"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['benefits'] ?? '' ?>" />
+          value="<?= $listing->benefits ?? '' ?>" />
       </div>
       <div class="mb-4">
         <input
@@ -64,7 +64,7 @@
           name="tags"
           placeholder="Tags"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['tags'] ?? '' ?>" />
+          value="<?= $listing->tags ?? '' ?>" />
       </div>
 
       <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
@@ -76,7 +76,7 @@
           name="company"
           placeholder="Company Name"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['company'] ?? '' ?>" />
+          value="<?= $listing->company ?? '' ?>" />
       </div>
       <div class="mb-4">
         <input
@@ -84,7 +84,7 @@
           name="address"
           placeholder="Address"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['address'] ?? '' ?>" />
+          value="<?= $listing->address ?? '' ?>" />
       </div>
       <div class="mb-4">
         <input
@@ -92,7 +92,7 @@
           name="city"
           placeholder="City"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['city'] ?? '' ?>" />
+          value="<?= $listing->city ?? '' ?>" />
       </div>
       <div class="mb-4">
         <input
@@ -100,7 +100,7 @@
           name="state"
           placeholder="State"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['state'] ?? '' ?>" />
+          value="<?= $listing->state ?? '' ?>" />
       </div>
       <div class="mb-4">
         <input
@@ -108,7 +108,7 @@
           name="phone"
           placeholder="Phone"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['phone'] ?? '' ?>" />
+          value="<?= $listing->phone ?? '' ?>" />
       </div>
       <div class="mb-4">
         <input
@@ -116,14 +116,14 @@
           name="email"
           placeholder="Email Address For Applications"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing['email'] ?? '' ?>" />
+          value="<?= $listing->email ?? '' ?>" />
       </div>
       <button
         class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
         Save
       </button>
       <a
-        href="/"
+        href="/listings/<?= $listing->id ?>"
         class="block text-center w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded focus:outline-none">
         Cancel
       </a>
