db6d64433132e6c021504ffb94ebb8c7ef3394df
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-11-12 15:36:47 +0400

    Insert listings

--- App/Controllers/ListingController.php
+++ App/Controllers/ListingController.php
@@ -92,7 +92,7 @@ class ListingController
     $newListingData['user_id'] = 1;
     $newListingData = array_map('sanitize', $newListingData);
 
-    $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+    $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
 
     $errors = [];
 
@@ -110,6 +110,32 @@ class ListingController
       ]);
     } else {
       // Submit data
+      $fields = [];
+
+      foreach ($newListingData as $field => $value) {
+        $fields[] = $field;
+      }
+
+      $fields = implode(', ', $fields);
+
+      $values = [];
+
+      foreach ($newListingData as $field => $value) {
+        // Convert empty strings to null
+        if ($value === '') {
+          $newListingData[$field] = null;
+        }
+
+        $values[] = ':' . $field;
+      }
+
+      $values = implode(', ', $values);
+
+      $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
+
+      $this->db->query($query, $newListingData);
+
+      redirect('/listings');
     }
   }
 }
